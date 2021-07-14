36904ab5bd240b832c435220841589cd2621e0cf
Shyamprasad Narapareddy <49093331+n-shyamprasad@users.noreply.github.com>
2021-07-14 10:58:24 +0800

    Add files via upload

---
+++ QCP.ChallengeApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using QCP.Component;
+using QCP.Model;
+namespace QCP.ChallengeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Load data from json
+            TradeData tradeData = new Utils().LoadTradeData("data.json");
+
+            Console.WriteLine("#########  Input  #########");
+            Utils.DisplayData(tradeData.OrderBook);
+
+            // Opening Price calculation
+            List<OrderBook> orderBooks = new Process().ProcessData(tradeData);
+           
+            Console.WriteLine("\n\n");
+            Console.WriteLine("#########  Output  #########");
+            Utils.DisplayData(orderBooks);
+
+            Console.ReadLine();
+        }
+    }
+}
