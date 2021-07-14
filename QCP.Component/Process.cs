a5dbec231432d97a5327f8e0f702a702f303caf0
Shyamprasad Narapareddy <49093331+n-shyamprasad@users.noreply.github.com>
2021-07-14 10:54:41 +0800

    Add files via upload

---
+++ QCP.Component/Process.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using QCP.Model;
+
+namespace QCP.Component
+{
+    public class Process
+    {
+        public List<OrderBook> ProcessData(TradeData tradeData)
+        {
+            List<Order> TempOrder = new List<Order>();           
+            List<Order> OtherTradeData = new List<Order>();
+            OrderBook orderBook = new OrderBook();
+            List<OrderBook> OutputList = new List<OrderBook>();
+            int iBuy = 0, iSell = 0;
+
+            foreach (OrderBook ob in tradeData.OrderBook)
+            {
+                orderBook = ob;
+
+                foreach (Order o in ob.Orders)
+                {
+                    if (o.Sell.Price > o.Buy.Price)
+                    {
+                        o.SetHigh = true;
+                        OtherTradeData = TempOrder;
+                        Order order = null;
+                        if (OtherTradeData.Count > 0 && !OtherTradeData[OtherTradeData.Count - 1].SetOpening && !OtherTradeData[OtherTradeData.Count - 1].SetHigh)
+                        {
+                            foreach (Order to in OtherTradeData)
+                            {
+                                if (!to.SetOpening)
+                                {
+                                    iBuy += to.Buy.Volume;
+                                    iSell += to.Sell.Volume;
+                                    order = OtherTradeData[OtherTradeData.Count - 1];
+                                }
+                                else
+                                    break;
+                            }
+
+
+                            for (int i = (OtherTradeData.Count - 1); i >= 0; i--)
+                            {
+                                if (!OtherTradeData[i].SetOpening)
+                                {
+                                    OtherTradeData.RemoveAt(i);
+                                }
+                                else
+                                    break;
+                            }
+
+                            if (order != null)
+                            {
+                                order.Buy.Price = order.Sell.Price;
+                                order.Buy.Volume = iSell;
+                                order.Sell.Volume = iSell;
+                                order.SetOpening = true;
+                                OtherTradeData.Add(order);
+                                o.Buy.Volume = (o.Buy.Volume + iBuy) - iSell;
+                            }
+                        }
+                    }
+                    TempOrder.Add(o);
+                }
+                orderBook.Orders.Clear();
+                orderBook.Orders.AddRange(OtherTradeData);
+                OutputList.Add(orderBook);
+            }
+            return OutputList;
+        }
+    }
+}
---
+++ QCP.Component/Utils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using QCP.Model;
+
+namespace QCP.Component
+{
+    public class Utils
+    {
+        public TradeData LoadTradeData(string filename)
+        {
+            using (StreamReader r = new StreamReader(filename))
+            {
+                string json = r.ReadToEnd();
+                TradeData tradeData = TradeData.FromJson(json);
+                return tradeData;
+            }
+        }
+        
+        public static void DisplayData(List<OrderBook> orderBook)
+        {
+            foreach (OrderBook ob in orderBook)
+            {
+                Console.WriteLine("SecurityCode: {0}", ob.SecurityCode);
+                Console.WriteLine("|Buy\t\t\t\t\t\t|Sell\t\t\t\t\t\t|");
+                Console.WriteLine("|-----------------------------------------------------------------------------------------------|");
+                Console.WriteLine("|Volume\t\t\t|Price\t\t\t|Price\t\t\t|Volume\t\t\t|");
+                Console.WriteLine("|-----------------------------------------------------------------------------------------------|");
+
+                foreach (Order o in ob.Orders)
+                {
+                    Console.WriteLine("|{0}\t\t\t|{1}\t\t\t|{2}\t\t\t|{3}\t\t\t|", o.Buy.Volume, o.Buy.Price, o.Sell.Price, o.Sell.Volume);
+                }
+                Console.WriteLine("|-----------------------------------------------------------------------------------------------|");
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
